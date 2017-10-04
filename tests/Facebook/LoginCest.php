7799c7a03411000a6383b32765f2b027fa951ad4
Bryan Estrito <bryan.estrito@gmail.com>
2017-10-05 03:56:39 +0800

    Working auto accept friend request

--- tests/Facebook/LoginCest.php
+++ tests/Facebook/LoginCest.php
@@ -1,25 +1,44 @@
 <?php
 
-
 class LoginCest
 {
-    // public function _before(FacebookTester $I)
-    // {
-    // }
-
-    // public function _after(FacebookTester $I)
-    // {
-    // }
-
     // tests
     public function tryToLogin(FacebookTester $I)
     {
         $I->amOnPage('/');
         $I->fillField('input#email', '');
         $I->fillField('input#pass', '');
-        $I->wait(2);
-        $I->click('form#login_form input[type=submit]');
-        $I->pauseExecution();
+        $I->click('Log In');
 
+        $this->friendsList($I);
+    }
+
+    private function reload($I)
+    {
+        $I->reloadPage();
+
+        $this->friendsList($I);
+    }
+
+    private function friendsList($I)
+    {
+        // click friends icon notif on navbar
+        $I->waitForElementVisible('//*[@id="fbRequestsJewel"]/a/div');
+        $I->click('//*[@id="fbRequestsJewel"]/a/div');
+
+        // wait for friend requests lists
+        $I->waitForElementVisible('//div[@class="fbRequestList"]', 10);
+
+        // visible friend requests
+        $friendRequests = $I->grabMultiple('//li[@class="objectListItem"]//form//button[1]');
+
+        // codecept_debug($friendRequests);
+
+        foreach ($friendRequests as $friendRequest) {
+            $I->click($friendRequest);
+            $I->wait(5);
+        }
+
+        $this->reload($I);
     }
 }
